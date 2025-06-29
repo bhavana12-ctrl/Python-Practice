aceeec373d9c8a3651287e19e307718653f4494c
bhavana12-ctrl <bskumavat370222@kkwagh.edu.in>
2025-06-29 10:49:46 +0530

    Create variables_and_datatypes.py

---
+++ variables_and_datatypes.py
@@ -0,0 +1,11 @@
+name = "Bhavana"
+age = 20
+height = 5.4
+is_student = True
+
+print("Name:", name)
+print("Age:", age)
+print("Height:", height)
+print("Is Student?", is_student)
+
+print("Datatype of age:", type(age))
